d83f2b70b8288753f0d800ad754331f33e21145b
GAF00 <gilangaditiya041@gmail.com>
2024-07-13 13:40:56 +0700

    menambahkan footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer id="footer" class="footer">
+    <div class="text-center position-relative">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Berita</span></strong>. All Rights Reserved
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short text-white"></i></a>
+
+<div id="preloader"></div>
+
+<script src="home/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="home/assets/vendor/aos/aos.js"></script>
+<script src="home/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="home/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="home/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="home/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="home/assets/vendor/php-email-form/validate.js"></script>
+
+<script src="home/assets/js/main.js"></script>
+
+</body>
+
+</html>
