68b1f73141746bce863c2be997c4f4faa2555e4c
upianandsyah30 <achmadluthfi312@gmail.com>
2024-07-13 14:23:55 +0700

    halaman-awal

---
+++ admin/index.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+//koneksi ke database
+include '../koneksi.php';
+
+if (!isset($_SESSION['admin'])) {
+    echo "<script>alert('Anda Harus Login');</script>";
+    echo "<script>location='login.php';</script>";
+    header('location:login.php');
+    exit();
+}
+function tanggal($tgl)
+{
+    $tanggal = substr($tgl, 8, 2);
+    $bulan = getBulan(substr($tgl, 5, 2));
+    $tahun = substr($tgl, 0, 4);
+    return $tanggal . ' ' . $bulan . ' ' . $tahun;
+}
+function getBulan($bln)
+{
+    switch ($bln) {
+        case 1:
+            return "Januari";
+            break;
+        case 2:
+            return "Februari";
+            break;
+        case 3:
+            return "Maret";
+            break;
+        case 4:
+            return "April";
+            break;
+        case 5:
+            return "Mei";
+            break;
+        case 6:
+            return "Juni";
+            break;
+        case 7:
+            return "Juli";
+            break;
+        case 8:
+            return "Agustus";
+            break;
+        case 9:
+            return "September";
+            break;
+        case 10:
+            return "Oktober";
+            break;
+        case 11:
+            return "November";
+            break;
+        case 12:
+            return "Desember";
+            break;
+    }
+}
+?>
