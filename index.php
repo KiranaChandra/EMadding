fe4a7fc7f7dc2316ec72c0d9d96d5e7e3a9a9cc6
upianandsyah30 <achmadluthfi312@gmail.com>
2024-07-14 20:30:38 +0700

    update menambahkan style

--- index.php
+++ index.php
@@ -4,49 +4,62 @@ include 'koneksi.php';
 ?>
 
 <?php include 'header.php'; ?>
-<main id="main">
+<section id="hero" class="hero">
 
-	<div class="breadcrumbs d-flex align-items-center" style="background-image: url('home/assets/img/bgutama.jpg');">
-		<div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+	<div class="info d-flex align-items-center">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center">
+					<h2 data-aos="fade-down">Selamat Datang Di Website <span>Berita</span></h2>
 
-			<h2>Berita</h2>
-			<ol>
-				<li><a href="index.php">Home</a></li>
-				<li>Berita</li>
-			</ol>
-
+				</div>
+			</div>
 		</div>
 	</div>
+	<div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
 
-	<section id="recent-blog-posts" class="recent-blog-posts">
-		<div class="container" data-aos="fade-up">
-  <div class=" section-header">
-			<h2>Berita</h2>
+		<div class="carousel-item active" style="background-image: url(home/assets/img/bgutama.jpg)">
 		</div>
+		<div class="carousel-item" style="background-image: url(home/assets/img/bgutama.jpg)"></div>
+		<a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+		</a>
+		<a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+			<span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+		</a>
+	</div>
 
-		<div class="row gy-5">
-			<?php $ambil = $koneksi->query("SELECT *FROM berita"); ?>
-			<?php while ($perberita = $ambil->fetch_assoc()) { ?>
-				<div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-					<div class="post-item position-relative h-100">
+</section>
+<section id="recent-blog-posts" class="recent-blog-posts">
+	<div class="container" data-aos="fade-up">
+  <div class=" section-header">
+		<h2>Berita</h2>
+	</div>
 
-						<div class="post-im g position-relative overflow-hidden">
-							<img src="foto/<?php echo $perberita['foto'] ?>" class="img-fluid" alt="">
-							<span class="post-date"><?= $perberita['tanggal'] ?></span>
-						</div>
+	<div class="row gy-5">
+		<?php $ambil = $koneksi->query("SELECT *FROM berita limit 3"); ?>
+		<?php while ($perberita = $ambil->fetch_assoc()) { ?>
+			<div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+				<div class="post-item position-relative h-100">
 
-						<div class="post-content d-flex flex-column">
+					<div class="post-img position-relative overflow-hidden">
+						<img src="foto/<?php echo $perberita['foto'] ?>" class="img-fluid" alt="">
+						<span class="post-date"><?= $perberita['tanggal'] ?></span>
+					</div>
 
-							<h3 class="post-title"><?= $perberita['judul'] ?></h3>
-							<hr>
-							<a href="beritadetail.php?id=<?php echo $perberita['idberita']; ?>" class="readmore stretched-link"><span>Baca Selengkapnya</span><i class="bi bi-arrow-right"></i></a>
-						</div>
+					<div class="post-content d-flex flex-column">
+
+						<h3 class="post-title"><?= $perberita['judul'] ?></h3>
+						<hr>
+						<a href="beritadetail.php?id=<?php echo $perberita['idberita']; ?>" class="readmore stretched-link"><span>Baca Selengkapnya</span><i class="bi bi-arrow-right"></i></a>
 					</div>
 				</div>
-			<?php } ?>
-		</div>
-		</div>
-	</section>
+			</div>
+		<?php } ?>
+	</div>
+	</div>
+</section>
+
 </main>
 <?php
 include 'footer.php';
